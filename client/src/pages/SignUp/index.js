a1ddf6914026913519f768df6e4b1a96eab4088f
Luke Evans <lukeevanstax@gmail.com>
2020-10-17 10:59:33 -0400

    update signup page with CSS styling

--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -39,54 +39,57 @@ const SignUp = () => {
   };
 
   return (
-    <div id="signup-container">
-     Free B-Roll by <a href="http://videezy.com">Videezy</a>
-      <video autoPlay loop muted
-      style={{
-        position: "absolute",
-        width: "100%",
-        left: "50%",
-        top: '50%',
-        height: "100%",
-        objectFit: "cover",
-        transform: "translate(-50%, -50%)",
-        zIndex: "-1"
-      }}
-      >
-      <source src={tourists} type="video/mp4"/>
-    </video>
-    <div className="text-center">
-      <h4>Sign Up</h4>
-      <form className="form-signin">
-        <label htmlFor="inputEmail" className="sr-only">
-          Email address
+    <div>
+      <div class="videezyStyling"> Free B-Roll by <a href="http://videezy.com">Videezy</a></div>
+      <div id="signup-container">
+        <video autoPlay loop muted
+          style={{
+            position: "absolute",
+            width: "100%",
+            left: "50%",
+            top: '50%',
+            height: "100%",
+            objectFit: "cover",
+            transform: "translate(-50%, -50%)",
+            zIndex: "-1"
+          }}
+        >
+          <source src={tourists} type="video/mp4" />
+        </video>
+        <div className="text-center">
+          <h1><span class="politi">Politi</span>
+            <span class="candid">Candid</span></h1>
+          <form className="form-signin">
+            <label htmlFor="inputEmail" className="sr-only">
+              Email address
         </label>
-        <input
-          type="email"
-          id="inputEmail"
-          className="form-control"
-          name="username"
-          placeholder="Email address"
-          value={signUpCreds.username}
-          onChange={handleChange}
-        />
-        <label htmlFor="inputPassword" className="sr-only">
-          Password
+            <input
+              type="email"
+              id="inputEmail"
+              className="form-control"
+              name="username"
+              placeholder="Email address"
+              value={signUpCreds.username}
+              onChange={handleChange}
+            />
+            <label htmlFor="inputPassword" className="sr-only">
+              Password
         </label>
-        <input
-          type="password"
-          id="inputPassword"
-          className="form-control"
-          name="password"
-          placeholder="Password"
-          value={signUpCreds.password}
-          onChange={handleChange}
-        />
-        <button className="btn btn-lg btn-primary btn-block" type="submit" onClick={handleSubmit}>
-          Sign Up
+            <input
+              type="password"
+              id="inputPassword"
+              className="form-control"
+              name="password"
+              placeholder="Password"
+              value={signUpCreds.password}
+              onChange={handleChange}
+            />
+            <button className="signupBtn btn btn-lg btn-primary btn-block" type="submit" onClick={handleSubmit}>
+              Sign Up
         </button>
-      </form>
-    </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
